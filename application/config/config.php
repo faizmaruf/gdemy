926790860dd69dc0c287e67dcb0f025ba7dcf004
Faiz Ma'ruf <faiz.maruf8888@gmail.com>
2020-07-14 06:16:42 +0700

    ubah baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gdemy-faiz/';
+$config['base_url'] = 'http://localhost:8888/gdemy/';
 
 /*
 |--------------------------------------------------------------------------
