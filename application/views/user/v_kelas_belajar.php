6c7d80509881ed43f1d8908e0e127d39572f007a
Faiz Ma'ruf <faiz.maruf8888@gmail.com>
2020-07-15 07:45:16 +0700

    menambahkan file kelas_belajar.css

--- application/views/user/v_kelas_belajar.php
+++ application/views/user/v_kelas_belajar.php
@@ -7,6 +7,7 @@
     <title>Gdemy</title>
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/bootstrap.css' ?>">  
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/custom.css' ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/kelas_belajar.css' ?>">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
